39f08f3e6a6b3900cc59402916542e5ad6dbbd6f
ilham <ilham.prastiyo@bti.id>
2024-12-15 17:37:54 +0700

    add uas-alpro

--- index.php
+++ index.php
@@ -1,88 +1,194 @@
+<?php
+session_start();
+// Jika tidak bisa login maka balik ke login.php
+// jika masuk ke halaman ini melalui url, maka langsung menuju halaman login
+if (!isset($_SESSION['login'])) {
+    header('location:login.php');
+    exit;
+}
+
+// Memanggil atau membutuhkan file function.php
+require 'function.php';
+
+// Menampilkan semua data dari table siswa berdasarkan nis secara Descending
+$siswa = query("SELECT * FROM siswa ORDER BY nis DESC");
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <!-- Data Tables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <!-- Font Google -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <!-- Own CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>PHP Native | CRUD</title>
 </head>
-<title>
-    Radhen Adebos</title>
+
 <body>
-    <nav class="navbar navbar-dark bg-dark">
-            <span class="navbar-brand mb-0 h1">RADHEN ADEBOS TUTORIAL</span>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-uppercase">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">PHP Native | CRUD</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
-<div class="container">
-    <br>
-    <h4><center>DAFTAR PESERTA PELATIHAN</center></h4>
-<?php
+    <!-- Close Navbar -->
 
-    include "koneksi.php";
+    <!-- Container -->
+    <div class="container">
+        <div class="row my-2">
+            <div class="col-md">
+                <h3 class="text-center fw-bold text-uppercase">Data Siswa</h3>
+                <hr>
+            </div>
+        </div>
+        <div class="row my-2">
+            <div class="col-md">
+                <a href="addData.php" class="btn btn-primary"><i class="bi bi-person-plus-fill"></i>&nbsp;Tambah Data</a>
+                <a href="export.php" target="_blank" class="btn btn-success ms-1"><i class="bi bi-file-earmark-spreadsheet-fill"></i>&nbsp;Ekspor ke Excel</a>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="col-md">
+                <table id="data" class="table table-striped table-responsive table-hover text-center" style="width:100%">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Umur</th>
+                            <th>Jurusan</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no = 1; ?>
+                        <?php foreach ($siswa as $row) : ?>
+                            <tr>
+                                <td><?= $no++; ?></td>
+                                <td><?= $row['nama']; ?></td>
+                                <td><?= $row['jekel']; ?></td>
+                                <?php
+                                $now = time();
+                                $timeTahun = strtotime($row['tgl_Lahir']);
+                                $setahun = 31536000;
+                                $hitung = ($now - $timeTahun) / $setahun;
+                                ?>
+                                <td><?= floor($hitung); ?> Tahun</td>
+                                <td><?= $row['jurusan']; ?></td>
+                                <td>
+                                    <button class="btn btn-success btn-sm text-white detail" data-id="<?= $row['nis']; ?>" style="font-weight: 600;"><i class="bi bi-info-circle-fill"></i>&nbsp;Detail</button> |
 
-    //Cek apakah ada kiriman form dari method post
-    if (isset($_GET['id_peserta'])) {
-        $id_peserta=htmlspecialchars($_GET["id_peserta"]);
+                                    <a href="ubah.php?nis=<?= $row['nis']; ?>" class="btn btn-warning btn-sm" style="font-weight: 600;"><i class="bi bi-pencil-square"></i>&nbsp;Ubah</a> |
 
-        $sql="delete from peserta where id_peserta='$id_peserta' ";
-        $hasil=mysqli_query($kon,$sql);
+                                    <a href="hapus.php?nis=<?= $row['nis']; ?>" class="btn btn-danger btn-sm" style="font-weight: 600;" onclick="return confirm('Apakah anda yakin ingin menghapus data <?= $row['nama']; ?> ?');"><i class="bi bi-trash-fill"></i>&nbsp;Hapus</a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- Close Container -->
 
-        //Kondisi apakah berhasil atau tidak
-            if ($hasil) {
-                header("Location:index.php");
+    <!-- Modal Detail Data -->
+    <div class="modal fade" id="detail" tabindex="-1" aria-labelledby="detail" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title fw-bold text-uppercase" id="detail">Detail Data Siswa</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-center" id="detail-siswa">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Close Modal Detail Data -->
 
-            }
-            else {
-                echo "<div class='alert alert-danger'> Data Gagal dihapus.</div>";
+    <!-- Footer -->
+    <div class="container-fluid">
+        <div class="row bg-dark text-white">
+            <div class="col-md-6 my-2" id="about">
+                <h4 class="fw-bold text-uppercase">About</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolore sed porro modi mollitia quaerat? Nam, error fugit sed, maiores illum architecto, officiis voluptate nesciunt voluptatibus aut reprehenderit perspiciatis doloremque!</p>
+            </div>
+            <div class="col-md-6 my-2 text-center link">
+                <h4 class="fw-bold text-uppercase">Account Links</h4>
+                <a href="https://web.facebook.com/akunlu/" target="_blank"><i class="bi bi-facebook fs-3"></i></a>
+                <a href="https://github.com/akunlu" target="_blank"><i class="bi bi-github fs-3"></i></a>
+                <a href="https://www.instagram.com/akunlu/" target="_blank"><i class="bi bi-instagram fs-3"></i></a>
+                <a href="https://twitter.com/akunlu" target="_blank"><i class="bi bi-twitter fs-3"></i></a>
+            </div>
+        </div>
+    </div>
+    <footer class="bg-dark text-white text-center" style="padding: 5px;">
+        <p>Created with <i class="bi bi-suit-heart-fill" style="color: red;"></i> by <u style="color: #fff;">namalu</u></p>
+    </footer>
+    <!-- Close Footer -->
 
-            }
-        }
-?>
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
+    <!-- Data Tables -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap5.min.js"></script>
 
-     <tr class="table-danger">
-            <br>
-        <thead>
-        <tr>
-       <table class="my-3 table table-bordered">
-            <tr class="table-primary">           
-            <th>No</th>
-            <th>Nama</th>
-            <th>Sekolah</th>
-            <th>Jurusan</th>
-            <th>No Hp</th>
-            <th>Alamat</th>
-            <th colspan='2'>Aksi</th>
+    <script>
+        $(document).ready(function() {
+            // Fungsi Table
+            $('#data').DataTable();
+            // Fungsi Table
 
-        </tr>
-        </thead>
-
-        <?php
-        include "koneksi.php";
-        $sql="select * from peserta order by id_peserta desc";
-
-        $hasil=mysqli_query($kon,$sql);
-        $no=0;
-        while ($data = mysqli_fetch_array($hasil)) {
-            $no++;
-
-            ?>
-            <tbody>
-            <tr>
-                <td><?php echo $no;?></td>
-                <td><?php echo $data["nama"]; ?></td>
-                <td><?php echo $data["sekolah"];   ?></td>
-                <td><?php echo $data["jurusan"];   ?></td>
-                <td><?php echo $data["no_hp"];   ?></td>
-                <td><?php echo $data["alamat"];   ?></td>
-                <td>
-                    <a href="update.php?id_peserta=<?php echo htmlspecialchars($data['id_peserta']); ?>" class="btn btn-warning" role="button">Update</a>
-                    <a href="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?id_peserta=<?php echo $data['id_peserta']; ?>" class="btn btn-danger" role="button">Delete</a>
-                </td>
-            </tr>
-            </tbody>
-            <?php
-        }
-        ?>
-    </table>
-    <a href="create.php" class="btn btn-primary" role="button">Tambah Data</a>
-</div>
+            // Fungsi Detail
+            $('.detail').click(function() {
+                var dataSiswa = $(this).attr("data-id");
+                $.ajax({
+                    url: "detail.php",
+                    method: "post",
+                    data: {
+                        dataSiswa,
+                        dataSiswa
+                    },
+                    success: function(data) {
+                        $('#detail-siswa').html(data);
+                        $('#detail').modal("show");
+                    }
+                });
+            });
+            // Fungsi Detail
+        });
+    </script>
 </body>
+
 </html>
